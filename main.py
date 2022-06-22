d678d30d62182311fa37a4c90bdf6edb6168a4b6
Arseniy Zemerov <62257322+NeXu7@users.noreply.github.com>
2022-06-22 19:38:43 +0300

    fix save problem

---
+++ main.py
@@ -0,0 +1,59 @@
+import os
+
+import streamlit as st
+from stqdm import stqdm
+import pandas as pd
+import numpy as np
+import utils.utils as utils
+import json
+
+st.title("Cell classifier")
+model_select = st.selectbox(
+    "Choose model architecture:",
+    ["", "densenet121", "densenet201", "efficientnet_b0"]
+)
+finish = False
+uploaded_file = None
+load_file = st.checkbox("Classification from geojson?", key=0)
+# download_prob = st.checkbox("Result as probability?", key=1)
+if load_file:
+    uploaded_file = st.file_uploader("Choose a file")
+
+if uploaded_file is not None:
+    uploaded_file = pd.read_json(uploaded_file).iloc[:10]
+    download_mask = uploaded_file.copy()
+
+
+if len(model_select) > 1:
+    if uploaded_file is not None:
+        uploaded_file = utils.contour_ext(uploaded_file)
+        st.write(f"{model_select}-classification in progress")
+        progress_bar = st.sidebar.progress(0)
+        status_text = st.sidebar.empty()
+        model = utils.get_model(model_select)
+        data = utils.get_data(slide_path="slides/5189/TCGA-BP-5189-01Z-00-DX1.cfffe175-da76-4edd-9187-0570a877127b.svs",
+                              counter_data=uploaded_file)
+        result = utils.classify_cell(model, data)
+        st.write(f"Готово!")
+        finish = True
+    elif not load_file:
+        st.write(f"{model_select}-classification in progress")
+        progress_bar = st.sidebar.progress(0)
+        status_text = st.sidebar.empty()
+        model = utils.get_model(model_select)
+        data = utils.get_data(img_folder_path="slides/test_cell_imgs/cell_img")
+        result = utils.classify_cell(model, data)
+        st.write(f"Готово!")
+        finish = True
+
+    if finish:
+        if uploaded_file is not None:
+            file_name = st.text_input("Input file name")
+            with open(f'{file_name}.json', 'w') as f:
+                json.dump(utils.make_predict_file(download_mask, result).to_dict(), f)
+        else:
+            pd.DataFrame([os.listdir("slides/test_cell_imgs/cell_img"),
+                          result.argmax(0)], columns=['file_name', 'predict'])
+        # elif download_prob:
+        #     st.download_button("Download result", result)
+st.button("Re-run")
---
+++ utils/utils.py
@@ -0,0 +1,173 @@
+import os
+from stqdm import stqdm
+import numpy as np
+import cv2 as cv
+import pandas as pd
+from scipy import ndimage
+import torch
+from torch.utils.data import Dataset
+from torchvision import models, transforms
+import slideio as sio
+from PIL import Image
+
+
+class ImgData(Dataset):
+    def __init__(self, file_names, file_dir):
+        self.file_names = file_names
+        self.file_dir = file_dir
+        mean = [0.65, 0.49, 0.64]
+        std = [0.16, 0.20, 0.20]
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize((128, 128)),
+            transforms.Normalize(mean, std)
+        ])
+
+    def __len__(self):
+        return len(self.file_names)
+
+    def __getitem__(self, item):
+        img = Image.open(os.path.join(self.file_dir, self.file_names[item]))
+        images = [img.rotate(i * 180) for i in range(2)]
+        images = [torch.unsqueeze(self.transform(img), dim=0) for img in images]
+        images = torch.cat(images)
+        return images
+
+
+class SlideData(Dataset):
+    def __init__(self, slide, contours):
+        self.slide = slide
+        self.contours = contours
+        mean = [0.65, 0.49, 0.64]
+        std = [0.16, 0.20, 0.20]
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Resize((128, 128)),
+            transforms.Normalize(mean, std)
+        ])
+
+    def __len__(self):
+        return len(self.contours)
+
+    def __getitem__(self, item):
+        img = self.cell_mask(self.contours[item], self.slide, pix_around=10)
+        h, w, _ = img.shape
+        top = (256 - h) // 2
+        left = (256 - w) // 2
+        img = cv.copyMakeBorder(img, top, top, left, left, cv.BORDER_CONSTANT, None, [0, 0, 0])
+        img_batch = [torch.unsqueeze(self.transform_img(img, i * 180, self.transform),
+                                     dim=0) for i in range(2)]
+        img_batch = torch.cat(img_batch)
+        return img_batch
+
+    @staticmethod
+    def cell_mask(cell_cont, image, pix_around=10, x_offset=0, y_offset=0):
+        cell_cont = np.array(cell_cont)
+        x_img, y_img, w, h = cv.boundingRect(cell_cont)
+        x = x_img - pix_around - x_offset
+        y = y_img - pix_around - y_offset
+        w = w + 2 * pix_around
+        h = h + 2 * pix_around
+        cell_img = image[y:y + h, x:x + w].copy()
+        cell_cont = cell_cont - [x_img - pix_around, y_img - pix_around]
+        mask = np.zeros(cell_img.shape[:2])
+        mask = cv.drawContours(mask, cell_cont, -1, (1), -1) + cv.drawContours(mask, cell_cont, -1, (1),
+                                                                               pix_around * 2)
+        mask = mask >= 1
+        cell_img[np.logical_not(mask)] = [0, 0, 0]
+        return cell_img
+
+    @staticmethod
+    def transform_img(img, angle, transform):
+        img = ndimage.rotate(img, angle)
+        img = transform(img)
+        return img
+
+
+def contour_ext(data):
+    data = data.dropna(axis=0)
+    measurements = []
+    for i in range(len(data)):
+        measurements.append(np.array(data['nucleusGeometry'].iloc[i]['coordinates'], dtype=np.int32))
+    return measurements
+
+
+def get_model(model_name):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    if model_name == "densenet121":
+        model = models.densenet121(pretrained=False)
+        model.classifier = torch.nn.Sequential(
+            torch.nn.Linear(model.classifier.in_features, 500),
+            torch.nn.Linear(500, 5)
+        )
+        model.load_state_dict(torch.load("models/densenet121.pt", map_location=device))
+        model.eval()
+        return model
+
+    if model_name == "densenet201":
+        model = models.densenet201(pretrained=False)
+        model.classifier = torch.nn.Sequential(
+            torch.nn.Linear(model.classifier.in_features, 500),
+            torch.nn.Dropout(p=0.2),
+            torch.nn.Linear(500, 5)
+        )
+        model.load_state_dict(torch.load("models/densenet201.pt", map_location=device))
+        model.eval()
+        return model
+
+    if model_name == "efficientnet_b0":
+        model = models.efficientnet_b0(pretrained=False)
+        model.classifier[1] = torch.nn.Linear(in_features=model.classifier[1].in_features,
+                                              out_features=5)
+        model.load_state_dict(torch.load("models/efficientnet_b0.pt", map_location=device))
+        model.eval()
+        return model
+
+
+def classify_cell(model, data):
+    softmax = torch.nn.Softmax(dim=1)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    voted_predict = []
+    with torch.no_grad():
+        for batch in stqdm(data, desc=f"Device: {device}. Progress"):
+            batch = batch.to(device)
+            predict = softmax(model(batch))
+            predict = predict.data.cpu().numpy()
+            voted_predict.append(predict.mean(0))
+    return np.array(voted_predict)
+
+
+def get_data(slide_path=None, counter_data=None, img_folder_path=None):
+    if slide_path is not None and counter_data is not None:
+        X = []
+        Y = []
+        for cont in counter_data:
+            x, y, w, h = cv.boundingRect(cont)
+            X.append(x)
+            Y.append(y)
+        size = (np.min(X) - 200, np.min(Y) - 200, np.max(X) - np.min(X) + 200, np.max(Y) - np.min(Y) + 200)
+        slide = sio.open_slide(slide_path, 'SVS')
+        scene = slide.get_scene(0)
+        slide = scene.read_block(size)
+        return SlideData(slide=slide, contours=counter_data)
+
+    if img_folder_path is not None:
+        img_names = [name for name in os.listdir(img_folder_path) if ".png" in name]
+        return ImgData(file_names=img_names, file_dir=img_folder_path)
+
+
+def make_predict_file(mask, predict):
+    ccd = {4: 16711680, 0: 255, 2: 16776960, 3: 16737280, 1: 6618880}
+    for i in range(len(predict)):
+        if predict[i, 0] > 0.3:
+            mask.loc[i, 'properties']['classification'] = {'name': 0,
+                                                           'colorRGB': ccd[0]
+                                                           }
+            mask.loc[i, 'properties']['measurements'] = [{'name': 'Grade', 'value': 0}]
+        else:
+            cell_class = int(np.argmax(predict[i]))
+            mask.loc[i, 'properties']['classification'] = {'name': cell_class,
+                                                           'colorRGB': ccd[cell_class]
+                                                           }
+            mask.loc[i, 'properties']['measurements'] = [{'name': 'Grade', 'value': cell_class}]
+    return mask
